a9020a7b7ca7935150785b9ab471433fa349e3f1
Rakesh Mistry <mistryrn@mcmaster.ca>
2014-11-01 00:46:37 -0400

    Added basic MailApp layout.

--- Examples/MailApp/MailApp/scripts/app.js
+++ Examples/MailApp/MailApp/scripts/app.js
@@ -1,5 +1,7 @@
 ﻿function Start() {
-    ApplyTemplate('navBar', data);
+    ApplyTemplate('navMenu', data);
+    ApplyTemplate('header', headers);
+    ApplyTemplate('mailList', jsonValues);
 }
 function ApplyTemplate(id, data) {
     var template = document.getElementById(id + 'Template');
@@ -8,4 +10,10 @@ function ApplyTemplate(id, data) {
 }
 var data = {
     inboxes: ['Inbox', 'Sent', 'Trash']
+};
+var headers = {
+    headings: ['Date', 'Subject', 'From', 'To']
+};
+var jsonValues = {
+    emails: [{date: 'October 31, 2014', subject: 'YHack Today!', from: 'YHack', to: 'Me'}, {date: 'October 30, 2014', subject: 'YHack Tomorrow!', from: 'YHack', to: 'Me'}]
 };
