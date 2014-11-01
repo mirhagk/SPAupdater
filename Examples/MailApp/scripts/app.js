474f1d5d86bab5fb9f1290680e120def2ecbc6e4
mirhagk <mirhagk@gmail.com>
2014-10-31 22:45:20 -0400

    Created basic mail app using doT.js

---
+++ Examples/MailApp/scripts/app.js
@@ -0,0 +1,11 @@
+﻿function Start() {
+    ApplyTemplate('navBar', data);
+}
+function ApplyTemplate(id, data) {
+    var template = document.getElementById(id + 'Template');
+    var target = document.getElementById(id);
+    target.innerHTML = doT.template(template.innerHTML)(data);
+}
+var data = {
+    inboxes: ['Inbox', 'Sent', 'Trash']
+};
