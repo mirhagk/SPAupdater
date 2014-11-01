90699fdb696b9d83cfbc33a6018bf4191a5d3844
Rakesh Mistry <mistryrn@mcmaster.ca>
2014-11-01 03:19:14 -0400

    Update MailApp demo to allow for live switching between mailboxes.

--- Examples/MailApp/MailApp/scripts/app.js
+++ Examples/MailApp/MailApp/scripts/app.js
@@ -6,22 +6,41 @@ function Start() {
 function RefreshTemplates() {
     ApplyTemplate('navMenu', data);
     ApplyTemplate('header', headers);
-    ApplyTemplate('mailList', jsonValues);
+    ApplyTemplate('mailList', inboxValues);
 }
+
+function Messages(id) {
+    var template = document.getElementById('mailListTemplate');
+    var target = document.getElementById('mailList');
+    if (id.text == 'Inbox')
+        target.innerHTML = doT.template(template.innerHTML)(inboxValues);
+    else if (id.text == 'Spam')
+        target.innerHTML = doT.template(template.innerHTML)(spamValues);
+    else
+        target.innerHTML = doT.template(template.innerHTML)(trashValues);
+}
+
 function ApplyTemplate(id, data) {
     var template = document.getElementById(id + 'Template');
     var target = document.getElementById(id);
     target.innerHTML = doT.template(template.innerHTML)(data);
 }
 var data = {
-    inboxes: ['Inbox', 'Sent', 'Trash']
+    inboxes: ['Inbox', 'Spam', 'Trash']
 };
 var headers = {
     headings: ['Date', 'Subject', 'From', 'To']
 };
-var jsonValues = {
+var inboxValues = {
     emails: [{date: 'October 31, 2014', subject: 'YHack Today!', from: 'YHack', to: 'Me'}, {date: 'October 30, 2014', subject: 'YHack Tomorrow!', from: 'YHack', to: 'Me'}]
 };
+var spamValues = {
+    emails: [{date: 'October 31, 2014', subject: 'YOU WIN!', from: 'LOTTO69', to: 'Me'}, {date: 'October 30, 2014', subject: 'Male Enhancement?', from: 'Dr Oz', to: 'Me'}]
+};
+var trashValues = {
+    emails: [{date: 'October 31, 2014', subject: 'Groupon Deals', from: 'Groupon', to: 'Me'}]
+};
+
 Util = {};
 Util.log = function (x) { console.log('foo'); }
 //updater.UpdateComponent({componentType: 1, name:'log', code:'console.log("bar");'})
